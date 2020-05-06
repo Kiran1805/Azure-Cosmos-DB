748c32b86f06046e742cf0b3cb2796ae1060b81b
Kiran1805 <52439814+Kiran1805@users.noreply.github.com>
2020-05-06 15:36:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlobFileShare
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string folder = @"D:\uploadpath\";
+            // Filename  
+            string fileName = "downloadfile.txt";
+            // Fullpath. You can direct hardcode it if you like.  
+            string fullPath = folder + fileName;
+            string containerName = "firstblobstoragr";
+            BlobFileShare blobFileShare = new BlobFileShare();
+            blobFileShare.uploadToBlob(fullPath, containerName);
+     
+            blobFileShare.downloadFromBlob(fileName, containerName);
+        }
+    }
+}
